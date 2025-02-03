bfce886f537f2ac39dc17f3fd48d279315d333de
Jang-YoonSung <jang064880@gmail.com>
2025-02-03 16:59:29 +0900

    기간 별 뉴스 .txt로 저장

--- crawling.py
+++ crawling.py
@@ -12,19 +12,19 @@ def crawling(start, end):
     end = datetime.strptime(end, "%Y-%m-%d")
     dates = [(start + timedelta(days=i)).strftime("%Y%m%d") for i in range((end-start).days+1)]
 
-    removeList = ["\n","\t","\r",'·', '...', '“', '”', '‘', '’', '(', ')', 'ㆍ', '…', ',', '-', '▶', '\'','"', 
-        'EPA','=','연합뉴스','로이터','AP','※','☎','>','기자','앵커','[앵커]', '[리포트]', 'Q.', 'A.', '.', '!', '? ', 
-        '(영상취재 :', '(영상편집 :', '[날씨]', ' : ', ']', '[',"/",
+    removeList = ["\n", "\t", "\r", '·', '...', '“', '”', '‘', '’', '(', ')', 'ㆍ', '…', ',', '-', '▶', '\'', '"', 
+        'EPA', '=', '연합뉴스', '로이터', 'AP', '※', '☎', '>', '기자', '앵커', '[앵커]', '[리포트]', 'Q.', 'A.', '.', '!', '? ', 
+        '(영상취재 :', '(영상편집 :', '[날씨]', ' : ', ']', '[', "/", ":",
         "1.","2.","3.","4.","5.","6.","7.","8.","9."]
     pattern = "|".join(map(re.escape, removeList))
-    print(pattern)
 
     driver = wb.Chrome()
     driver.get("https://broadcast.tvchosun.com/news/newspan/ch19.cstv")  # 웹사이트 URL 입력
     driver.implicitly_wait(10)
 
+    resultContent=[]
     for i in range(len(dates)):
-        if (datetime.strptime(dates[i], "%Y%m%d").weekday() <5) :
+        if datetime.strptime(dates[i], "%Y%m%d").weekday() < 5 :    
             # 1. calendar-picker가 안 보이는 경우 on으로 바꾸기
             calendar = driver.find_element(By.CLASS_NAME, "calendar-picker")
             driver.execute_script("arguments[0].classList.add('on');", calendar)  # on 상태로 변경
@@ -42,6 +42,8 @@ def crawling(start, end):
             driver.execute_script("arguments[0].click();", month_element)
 
             target_date = dates[i][6:]
+            if target_date[0] == '0':
+                target_date = target_date[1:]
             date_element = driver.find_element(By.XPATH, f"//ul[@class='date-list']//a[text()='{target_date}']")
             driver.execute_script("arguments[0].click();", date_element)
 
@@ -51,7 +53,6 @@ def crawling(start, end):
 
             links = [a.get_attribute("href") for a in driver.find_elements(By.CSS_SELECTOR, "ul.item-list li a")]
             
-            resultContent=[]
             for url in links:
                 response = requests.get(url)
                 html = response.text
@@ -62,8 +63,13 @@ def crawling(start, end):
                     clean_text = re.sub(pattern, "", text.get_text().strip())
                     print(clean_text)
 
-
                     resultContent.append(clean_text) #텍스트만 추출, html 태그는 제거
     driver.quit()
+    return resultContent
 
-crawling("2025-01-17", "2025-01-17")
+result = crawling("2025-01-17", "2025-01-30")
+
+file_name = './output_doc_20250131.txt'
+
+with open(file_name, 'w', encoding='UTF-8') as f:
+    f.write('\n'.join(result))
